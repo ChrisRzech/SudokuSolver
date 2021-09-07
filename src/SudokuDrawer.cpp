6c6526ed1ad86b1cf27447889ca9f2f40d70f91d
Christopher Rzechula <christopherrzechula@gmail.com>
2021-09-07 15:14:08 -0700

    Add sudoku drawer

---
+++ src/SudokuDrawer.cpp
@@ -0,0 +1,48 @@
+#include "SudokuDrawer.hpp"
+#include <iostream>
+
+SudokuDrawer::SudokuDrawer(const Sudoku& sudoku)
+    : sudoku{sudoku}
+{
+    //Do nothing
+}
+
+SudokuDrawer::SudokuDrawer(const Sudoku& sudoku, sf::Vector2f position, sf::Vector2u size)
+    : sudoku{sudoku}, position{position}, size{size}
+{
+    //Do nothing
+}
+
+std::pair<int, int> SudokuDrawer::convertPointToCell(sf::Vector2f point) const
+{
+    float spacingX = size.x / Sudoku::BOARD_SIZE;
+    float spacingY = size.y / Sudoku::BOARD_SIZE;
+    
+    point -= position;
+    
+    return std::make_pair(point.x / spacingX, point.y / spacingY);
+}
+
+void SudokuDrawer::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+    sf::VertexArray lines(sf::Lines, (Sudoku::BOARD_SIZE - 1) * 2);
+    float spacingX = size.x / Sudoku::BOARD_SIZE;
+    float spacingY = size.y / Sudoku::BOARD_SIZE;
+    float startingX = spacingX + position.x;
+    float startingY = spacingY + position.y;
+    
+    for(int i = 0; i < Sudoku::BOARD_SIZE - 1; i++)
+    {
+        sf::Color color = (i % Sudoku::BOX_SIZE == Sudoku::BOX_SIZE - 1 ? sf::Color::White : sf::Color{50, 50, 50});
+        
+        /* Vertical line */
+        lines.append(sf::Vertex{sf::Vector2f{startingX + spacingX * i, position.y}, color});
+        lines.append(sf::Vertex{sf::Vector2f{startingX + spacingX * i, position.y + size.y}, color});
+        
+        /* Horizontal line */
+        lines.append(sf::Vertex{sf::Vector2f{position.x, startingY + spacingY * i}, color});
+        lines.append(sf::Vertex{sf::Vector2f{position.x + size.x, startingY + spacingY * i}, color});
+    }
+    
+    target.draw(lines);
+}
---
+++ src/SudokuDrawer.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include "Sudoku.hpp"
+#include <SFML/Graphics.hpp>
+
+class SudokuDrawer : public sf::Drawable
+{
+public:
+    SudokuDrawer(const Sudoku&);
+    SudokuDrawer(const Sudoku&, sf::Vector2f position, sf::Vector2u size);
+    
+    std::pair<int, int> convertPointToCell(sf::Vector2f) const;
+    
+    void draw(sf::RenderTarget&, sf::RenderStates states) const override;
+    
+    sf::Vector2f position;
+    sf::Vector2u size;
+
+private:
+    const Sudoku& sudoku;
+};
