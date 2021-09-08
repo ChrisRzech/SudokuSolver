8cbd811b76b4ee198bef5f1e6768dae1526c06e8
Christopher Rzechula <christopherrzechula@gmail.com>
2021-09-08 12:30:27 -0700

    Prevent invalid indexing on sudoku board

--- src/Sudoku.cpp
+++ src/Sudoku.cpp
@@ -2,7 +2,8 @@
 
 void Sudoku::setValue(int row, int col, int value)
 {
-    m_board[row][col] = value;
+    if(0 < value && value <= Sudoku::BOARD_SIZE)
+        m_board[row][col] = value;
 }
 
 std::optional<int> Sudoku::getValue(int row, int col) const
