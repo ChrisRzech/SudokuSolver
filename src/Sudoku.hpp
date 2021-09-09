4b7fc31147b2fac68cdced9c671c005854f6b920
Christopher Rzechula <christopherrzechula@gmail.com>
2021-09-08 22:50:14 -0700

    Add clearing functions to sudoku board

--- src/Sudoku.hpp
+++ src/Sudoku.hpp
@@ -10,6 +10,9 @@ public:
     
     void setValue(int row, int col, int value);
     std::optional<int> getValue(int row, int col) const;
+    void clearValue(int row, int col);
+    
+    void clear();
 
 private:
     std::array<std::array<std::optional<int>, BOARD_SIZE>, BOARD_SIZE> m_board;
